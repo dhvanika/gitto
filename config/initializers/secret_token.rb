fee34e8e7126ed85bb48dd1b807cae4b1dc76e47
Mujtaba Badat <mujtaba_badat@hotmail.com>
2013-07-12 22:46:07 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GhettoBlaster::Application.config.secret_token = '6c8545875d448b2a7e409f7374761d0652549c61b2bc9d7d4e4246c73ff3b5347ed33de6ecbde5fbdb391e33c30335c9f5f475f042b63dc3ca03a0944657830f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GhettoBlaster::Application.config.session_store :cookie_store, key: '_ghetto_blaster_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GhettoBlaster::Application.config.session_store :active_record_store
